d1664af3a010afcb6af1b113e1c702663eacee2e
suresh9047 <suresh904753@gmail.com>
2024-11-16 16:50:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import HomeNav from './components/Navbar/HomeNav'
+import React from 'react';
+import HomeNav from './components/Navbar/HomeNav';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from '../src/pages/LoginPage'
+import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProfilePage from './pages/ProfilePage';
 import HomePage from './pages/HomePage';
@@ -11,26 +11,25 @@ import Coursedetail from './pages/Coursedetail';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const App = () => {
   return (
-    <div>
-      <BrowserRouter basename="/React_come">
-      <HomeNav/>
-      <Routes>
-           <Route path='/' element={<Navigate to="/HomePage" />}/>
-           <Route path='/HomePage' element={<HomePage/>}/>
-           <Route path='/LoginPage' element={<LoginPage/>}/>
-           <Route path='/SignupPage' element={<SignupPage/>}/>
-           <Route path='/ProfilePage' element={<ProfilePage/>}/>
-           <Route path='/CoursePage' element={<CoursePage/>}/>
-           <Route path='/CheckoutPage'element={<CheckoutPage/>}/>
-           <Route path='/Coursedetail'element={<Coursedetail/>}/>
-
-      </Routes>
+    <div className="app-container">
+      <BrowserRouter basename="/React_course">
+        <HomeNav />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/courses" element={<CoursePage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/coursedetail/:id" element={<Coursedetail />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
